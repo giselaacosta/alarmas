115bcd7f804a6b37041e136ac77ef0b8e6f7d973
Gisela Acosta <gacosta@technisys.com>
2020-09-18 08:17:08 -0300

    commit

---
+++ alarmas/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+    firebaseConfig : {
+    apiKey: "AIzaSyBcZiUMj2OqRbXV8B4ADrQHhgtACTCZlSM",
+    authDomain: "alarmas-46259.firebaseapp.com",
+    databaseURL: "https://alarmas-46259.firebaseio.com",
+    projectId: "alarmas-46259",
+    storageBucket: "alarmas-46259.appspot.com",
+    messagingSenderId: "170568104090",
+    appId: "1:170568104090:web:257206daed6e6ada7d4723",
+    measurementId: "G-5Y9RFT0WFG"
+  }
+};
---
+++ alarmas/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBcZiUMj2OqRbXV8B4ADrQHhgtACTCZlSM",
+    authDomain: "alarmas-46259.firebaseapp.com",
+    databaseURL: "https://alarmas-46259.firebaseio.com",
+    projectId: "alarmas-46259",
+    storageBucket: "alarmas-46259.appspot.com",
+    messagingSenderId: "170568104090",
+    appId: "1:170568104090:web:257206daed6e6ada7d4723",
+    measurementId: "G-5Y9RFT0WFG"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
